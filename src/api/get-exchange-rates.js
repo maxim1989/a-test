d4a8abdc0dcdd4994cec0bce82b2619526b7e40e
Fedorov Maksim <MLFedorov@sberbank.ru>
2021-01-03 19:37:04 +0300

    [main]: fix

--- src/api/get-exchange-rates.js
+++ src/api/get-exchange-rates.js
@@ -8,8 +8,10 @@ const calcConvertCombinations = (data) => {
     const store = [];
 
     for (let i = 0; i < keys.length; i++) {
-        for (let j = i + 1; j < keys.length; j++) {
-            store.push(`${keys[i]} => ${keys[j]}`);
+        for (let j = 0; j < keys.length; j++) {
+            if (i !== j) {
+                store.push(`${keys[i]} => ${keys[j]}`);
+            }
         }
     }
 
